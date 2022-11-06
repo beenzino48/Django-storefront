5e5813968dfc7a4155b15ca811331d1dceb961db
hamishliu <hamish.mc.liu@gmail.com>
2022-11-06 16:20:24 +0900

    added a POSTS table and migrated it

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -40,12 +40,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
-
 ROOT_URLCONF = 'storefront.urls'
 
 TEMPLATES = [
